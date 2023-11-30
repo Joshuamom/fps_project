4489cf7f87be95a1bbc2e954e2a164b18071a951
Joshuamom <jmom@iu.edu>
2023-11-30 12:56:30 -0500

    mouse moved

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-const MOUSE_SENSITIVITY = 0.001
+const MOUSE_SENSITIVITY = 0.050
 const MOUSE_RANGE = 1.2
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
