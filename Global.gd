9c66d1c6b28069c9db1519295bb429fc3aba9cdb
Joshuamom <jmom@iu.edu>
2023-12-04 11:36:35 -0500

    added camera model

--- Global.gd
+++ Global.gd
@@ -1,5 +1,14 @@
 extends Node
 
 func _unhandled_input(event):
-	if event.is_action_pressed("Quit"):
-		get_tree().quit()
+	if event.is_action_pressed("menu"):	
+		var menu = get_node_or_null("/root/game/menu")
+		if menu == null:
+			get_tree().quit()
+		else:
+			if not menu.visible:
+				menu.show()
+				get_tree().paused = true 	
+			else:
+				menu.hide()
+				get_tree().paused = false
