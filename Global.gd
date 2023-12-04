562abc76e1ee81d8246ba853143f7c1e073f456a
Joshuamom <jmom@iu.edu>
2023-12-04 14:51:42 -0500

    menu done

--- Global.gd
+++ Global.gd
@@ -1,18 +1,83 @@
+#extends Node
+#
+#func _ready():
+#	process_mode = PROCESS_MODE_ALWAYS
+#
+#func _unhandled_input(event):
+#	if event.is_action_pressed("pause"):
+#		var menu = get_node_or_null("/root/Game/pause")
+#		if menu == null:
+#			get_tree().quit()
+#		else:
+#			if not menu.visible:
+#				menu.show()
+#				get_tree().paused = true
+#			else:
+#				menu.hide()
+#				get_tree().paused = false
+#
+#extends Node
+#
+#var menu
+#
+#func _ready():
+#	process_mode = PROCESS_MODE_ALWAYS
+#	menu = get_node_or_null("/root/Game/pause")
+#
+#func _unhandled_input(event):
+#	if event.is_action_pressed("pause"):
+#		if menu == null:
+#			get_tree().quit()
+#		else:
+#			if not menu.visible:
+#				menu.show()
+#				set_process(false)  # Pause processing for the current node
+#			else:
+#				menu.hide()
+#				set_process(true)   # Resume processing for the current node
+
+#extends Node
+#
+#var menu
+#
+#func _ready():
+#	process_mode = PROCESS_MODE_ALWAYS
+#	menu = get_node_or_null("/root/Game/pause")
+#
+#func _unhandled_input(event):
+#	if event.is_action_pressed("pause"):
+#		if menu == null:
+#			get_tree().quit()
+#		else:
+#			if not menu.visible:
+#				menu.show()
+#				set_process(false)  # Pause processing for the current node
+#			else:
+#				menu.hide()
+#				set_process(true)   # Resume processing for the current node
+#				menu != null        # Set menu to null after hiding
+				
 extends Node
 
+var menu
+
 func _ready():
-	process_mode = PROCESS_MODE_ALWAYS	
+	process_mode = PROCESS_MODE_ALWAYS
+	menu = get_node_or_null("/root/Game/pause")
 
 func _unhandled_input(event):
-	if event.is_action_pressed("menu"):	
-		var menu = get_node_or_null("/root/Game/menu")
-		print("ovo")
+	if event.is_action_pressed("pause"):
+		menu = get_node_or_null("/root/Game/pause")
 		if menu == null:
-			get_tree().quit()
+			# If pause menu is not found, do nothing or handle it differently
+			print("Pause menu not found!")
 		else:
 			if not menu.visible:
 				menu.show()
-				get_tree().paused = true 	
+				set_process(false)  # Pause processing for the current node
 			else:
 				menu.hide()
-				get_tree().paused = false
+				set_process(true)   # Resume processing for the current node
+
+		
+
--- UI/Menu.gd
+++ UI/Menu.gd
@@ -1,10 +1,18 @@
 extends Control
 
+var menu = Global.get_node_or_null("/root/Game/pause")
+
 
 func _on_restart_pressed():
-	get_tree().paused = false
+#	get_tree().paused = false
+	print("o")
+#	get_tree().reload_current_scene()
 	get_tree().change_scene_to_file("res://game.tscn")
+
+
+
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+	
