55b564d0f2b7d5b271fc37699102872bf7137a73
Joshuamom <jmom@iu.edu>
2023-12-06 11:53:26 -0500

    not working

--- Player/camera.gd
+++ Player/camera.gd
@@ -1,15 +1,11 @@
 extends Node3D
-var vis = false
+
 func shoot():
-	$muzzel.show()
-	$detect.set_disabled(true)
-	var vis = true
+	$muzzel.show()	
 	$Timer.start()
 
 func _on_timer_timeout():
 	$muzzel.hide()
-	$detect.set_disabled(false)
-	var vis = false
 
 
 
--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -3,7 +3,6 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -33,4 +32,4 @@ func _physics_process(delta):
 
 func _on_det_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
 	if area.name == "detect":
-		print("fuck")
+		print("AAHHHHHHHHH")
