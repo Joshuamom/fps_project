d434d305c612075645c08c0a62e42bd92304bb23
Joshuamom <jmom@iu.edu>
2023-12-06 11:26:19 -0500

    AAAAAAAAAHHHHHHHH

--- Player/camera.gd
+++ Player/camera.gd
@@ -1,12 +1,15 @@
 extends Node3D
+var vis = false
 func shoot():
 	$muzzel.show()
-	$detect.show()
+	$detect.set_disabled(true)
+	var vis = true
 	$Timer.start()
 
 func _on_timer_timeout():
 	$muzzel.hide()
-	$detect.hide()
+	$detect.set_disabled(false)
+	var vis = false
 
 
 
